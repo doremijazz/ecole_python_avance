2f2b271196d077a85344bd70e2c15d212872d497
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-07 14:03:54 +0200

    Méthode read depuis la base de données

---
+++ ecole/daos/student_dao.py
@@ -0,0 +1,24 @@
+from models.student import Student
+from daos.dao import Dao
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class StudentDao(Dao[Student]):
+    def read(self, id_student: int) -> Optional[Student]:
+        student: Optional[Student]
+        with Dao.connection.cursor() as cursor:
+            sql = "SELECT * FROM student WHERE id_student=%s"
+            cursor.execute(sql, (id_student,))
+            record = cursor.fetchone()
+
+        if record is not None:
+            student = Student(
+                record["first_name"],
+                record["last_name"],
+                record["age"]
+            )
+            student.id = record["id_student"]
+        else:
+            student = None
+        return student
